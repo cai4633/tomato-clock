1cf38700a33403fa14b088d0123abdb262f12311
cai4633 <463353522@qq.com>
2020-10-08 16:40:59 +0800

    binding page

---
+++ miniprogram/components/tInput/tInput.js
@@ -0,0 +1,29 @@
+// components/tInput/tInput.js
+Component({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  },
+  properties:{
+    label: {
+      type: String,
+      value: 'name'
+    },
+    type: {
+      type: String,
+      value: 'text'
+    },
+    id: {
+      type: String,
+      value: 'id'
+    },
+    placeholder: {
+      type: String,
+      value: ''
+    },
+  }
+
+
+})
